777c8e86f136d4a1e3029357dbcce7032ded8a98
Monica Navas <54577@novasbe.pt>
2023-03-14 00:12:08 +0000

    changes to Notebook Monica and adding map network tab on APP

--- app/app.R
+++ app/app.R
@@ -7,10 +7,28 @@ library(DT)
 library(rjson)
 library(bslib)
 library(leaflet)
+library(dplyr)          
+library(ggmap)
+library(sf)
+library(geosphere)
+library(RColorBrewer)
+library(sp)
 
 # Load data --------------------------------------------------------------------
 dt.trade <- fread("../data/cleaned_trade_data.csv")
 json_data <- fromJSON(file = '../data/sample.json')
+
+#Prepare data for Map Network--------------------------------------------
+
+#Creating a list of unique countries SHOULD WE CHANGE THIS TO PARTNER NAME ?????????
+l.countries <- as.list(unique(dt.trade$reporter_name))
+
+#Creating a data table with the unique names of countries
+dt.country.coordinates <- dt.trade %>% distinct(partner_name, .keep_all = TRUE)
+
+#Selecting only the columns of country name , longitude and latitude and renaming them columns for future plotting
+dt.country.coordinates <- dt.country.coordinates[, c("partner_name", "partner_lat", "partner_long")]
+meta <- dt.country.coordinates %>% rename("name" = "partner_name", "lat" = "partner_lat", "lon" = "partner_long")
 
 #Prepare data for Compare Countries--------------------------------------------
 
@@ -72,9 +90,48 @@ ui <- fluidPage(
   
   navbarPage(
     title = "Network Analytics",
+    
+    tabPanel("Trade Network Map",  
+             sidebarLayout(
+               sidebarPanel(
+                 #Adding logo to sidebar
+                 img(src = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/World_Trade_Organization_%28logo_and_wordmark%29.svg/2560px-World_Trade_Organization_%28logo_and_wordmark%29.svg.png",width="100%"),
+                 #Introducing Inputs for the user to select type of trade, country, minimum trade value and years to plot 
+                 selectInput(inputId = "trader", 
+                    label = "Select Imports or Exports:",
+                    choices = c("Exports"="reporter_name","Imports"="partner_name"), 
+                    selected = "reporter_name"
+                    ),
+                 selectInput(inputId = "countrymap", 
+                    label = "Select Country:",
+                    choices = l.countries, 
+                    selected = "Hungary"
+                    ),
+                 sliderInput(inputId = "MinWeight",
+                    label = "Minimum Trade Value (USD):",
+                    min = 0,
+                    max = 20000000000,
+                    value = 0
+                    ),
+                 sliderInput(inputId = "year_range",
+                    label = "Select Year Range:",
+                    min = 2000,
+                    max = 2022,
+                    value = c(2018, 2022),
+                    step = 1)
+                 ),
+               # Output: Show network
+               mainPanel(
+                 leafletOutput("map1"),
+                 DTOutput("datamap")
+                 )
+               )
+             ),
+    
     tabPanel("Compare Countries",
              sidebarLayout(
                sidebarPanel(
+                 img(src = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/World_Trade_Organization_%28logo_and_wordmark%29.svg/2560px-World_Trade_Organization_%28logo_and_wordmark%29.svg.png", width = "100%"),
                  selectInput("country", "Country:", choices = c("", sort(unique(dt.merged$reporter_name))), selected = NULL),
                  selectInput("country2", "Country 2:", choices = c("", sort(unique(dt.merged$reporter_name))), selected = NULL),
                  selectInput("country3", "Country 3:", choices = c("", sort(unique(dt.merged$reporter_name))), selected = NULL),
@@ -101,6 +158,7 @@ ui <- fluidPage(
     tabPanel("Clusteranalyse",
              sidebarLayout(
                sidebarPanel(
+                 img(src = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/World_Trade_Organization_%28logo_and_wordmark%29.svg/2560px-World_Trade_Organization_%28logo_and_wordmark%29.svg.png", width = "100%"),
                  selectInput(
                    inputId = "filter_reporter",
                    label = "Reporter:",
@@ -199,6 +257,166 @@ ui <- fluidPage(
 )
 # Define server ----------------------------------------------------------------
 server <- function(input, output, session) {
+  
+  # Map output
+  output$map1 <- renderLeaflet({
+    
+    # Set the parameters of the inputs to select years, minimum trade, reporter and partner name
+    # This is done with an if statement depending if user selected Exports or Imports 
+    if(input$trader=="reporter_name"){
+      dt.trade.country <- dt.trade[ dt.trade$reporter_name == input$countrymap, ]
+      dt.trade.country.year <- dt.trade.country[dt.trade.country$year >= input$year_range[1] &
+                                                  dt.trade.country$year <= input$year_range[2], ]
+      dt.trade.country.year <- dt.trade.country.year[dt.trade.country.year$trade_value_usd >                                                      input$MinWeight, ]
+      
+    } else {
+      dt.trade.country <- dt.trade[ dt.trade$partner_name == input$countrymap, ]
+      dt.trade.country.year <- dt.trade.country[dt.trade.country$year >= input$year_range[1] &
+                                                  dt.trade.country$year <= input$year_range[2], ]
+      dt.trade.country.year <- dt.trade.country.year[dt.trade.country.year$trade_value_usd >                                                      input$MinWeight, ]
+      
+    }
+    
+    #creating dataframe from the data table and defining from and to parameters
+    df <- data.frame("from" = dt.trade.country.year$reporter_name,
+                     "to"= dt.trade.country.year$partner_name)
+    
+    #checking if there is data from the inputs demanded my the user, if not, map shows a message saying that no data is available
+    
+    if (nrow(df) == 0) {
+      popup <- paste0("No data available for selected country")
+      return(leaflet() %>% addTiles() %>% setView(lng = 0, lat = 0, zoom = 2) %>%
+               addPopups(lng = 0, lat = 0, popup = popup))
+    }
+    
+    # Creating graph with data frame where the vertices are the meta data table containing all the countries with their longitude and latitude as previously explained 
+    g <- graph_from_data_frame(df, directed = TRUE, vertices = meta)
+    
+    # Setting weights as the trade value 
+    g <- set_edge_attr(g, "weight", value = dt.trade.country.year$trade_value_usd)
+    
+    # Remove vertices with degree 0
+    g <- delete.vertices(g, which(degree(g) == 0))
+    
+    # Retrieving data frame form the graph to get the coordinates of the vertices by summing longitude and latitude
+    gg <- get.data.frame(g, "both")
+    vert <- gg$vertices
+    coordinates(vert) <- ~lon + lat
+    
+    # Creating spatial lines from the edges of the graph with the weight of the trade value as an attribute
+    
+    # Retrieving the edges of the graph as a data frame from the output of get.data.frame(g, "both").
+    edges <- gg$edges
+    
+    # Retrieving the weights of the edges from the attribute named "weight" in the graph g.
+    weights <- E(g)$weight
+    
+    # Creating a list of SpatialLines objects from the vertices data frame vert using the edges data frame. For each row i in the edges data frame, it extracts the "from" and "to" vertex names, retrieves the corresponding rows from vert, creates a SpatialLines object from these vertices, and stores it in the list.
+    
+    edges <- lapply(1:nrow(edges), function(i) {
+      as(rbind(vert[vert$name == edges[i, "from"], ],
+               vert[vert$name == edges[i, "to"], ]),
+         "SpatialLines")
+    })
+    
+    #This loop assigns unique IDs to the SpatialLines objects in the list edges using the spChFIDs function. The new IDs are character strings generated from the index i.
+    
+    for (i in seq_along(edges)) {
+      edges[[i]] <- spChFIDs(edges[[i]], as.character(i))
+    }
+    
+    # Combining the SpatialLines objects in the list edges into a single object of class SpatialLines.
+    edges <- do.call(rbind, edges)
+    
+    # Creating a data frame edges_df with two columns: "id" and "weight". The "id" column contains sequential integers, and the "weight" column contains the weights of the edges.
+    edges_df <- data.frame(id = seq_along(edges), weight = weights)
+    
+    # Combining the SpatialLines object edges with the data frame edges_df to create a SpatialLinesDataFrame. The resulting object contains both the spatial information (the lines connecting the vertices) and the attribute information (the weights of the edges)
+    edges <- SpatialLinesDataFrame(edges, data = edges_df)
+    
+    #defining color palette for weights on the map used below
+    pal <- colorNumeric(palette = "YlOrRd", domain = c(min(edges_df$weight), max(edges_df$weight)))
+    
+    
+    leaflet(vert) %>%
+      
+      #Format of the map
+      addProviderTiles("Stamen.Toner", options = providerTileOptions(noWrap = TRUE, zoomSnap = 0, 
+                                                                     attributionControl = FALSE, 
+                                                                     backgroundColor = "#f2f2f2",opacity = 
+                                                                       0.4)) %>%
+      #Adding red circles on edges
+      addCircleMarkers(data = vert, radius = 2,
+                       color = "red",
+                       fillColor = "red",
+                       fillOpacity = 0.8,
+                       stroke = FALSE) %>%
+      
+      #Adding lines connecting edges with a color scheme representing the different weights of the trades
+      addPolylines(data = edges, weight = 2, color = ~pal(weight)) %>%
+      
+      #Adding legend for color of the lines representing the weights of exports and import's trade value
+      addLegend(position = "bottomright", pal = pal, values = edges_df$weight, 
+                title = "Trade Value (USD)", labFormat = labelFormat(suffix = " USD", digits = 0)) 
+    
+  })
+  
+  #Centrality Table Output
+  output$datamap <- renderDT({
+    
+    if(input$trader=="reporter_name"){
+      dt.trade.country <- dt.trade[ dt.trade$reporter_name == input$countrymap, ]
+      dt.trade.country.year <- dt.trade.country[dt.trade.country$year >= input$year_range[1] &
+                                                  dt.trade.country$year <= input$year_range[2], ]
+      dt.trade.country.year <- dt.trade.country.year[dt.trade.country.year$trade_value_usd >                                                      input$MinWeight, ]
+      
+    } else {
+      dt.trade.country <- dt.trade[ dt.trade$partner_name == input$countrymap, ]
+      dt.trade.country.year <- dt.trade.country[dt.trade.country$year >= input$year_range[1] &
+                                                  dt.trade.country$year <= input$year_range[2], ]
+      dt.trade.country.year <- dt.trade.country.year[dt.trade.country.year$trade_value_usd >                                                      input$MinWeight, ]
+      
+    }
+    
+    #creating dataframe from the data table and defining from and to parameters
+    df <- data.frame("from" = dt.trade.country.year$reporter_name,
+                     "to"= dt.trade.country.year$partner_name)
+    
+    #checking if there is data from the inputs demanded my the user, if not, map shows a message saying that no data is available
+    
+    if (nrow(df) == 0) {
+      popup <- paste0("No data available for selected country")
+      return(leaflet() %>% addTiles() %>% setView(lng = 0, lat = 0, zoom = 2) %>%
+               addPopups(lng = 0, lat = 0, popup = popup))
+    }
+    
+    # Creating graph with data frame where the vertices are the meta data table containing all the countries with their longitude and latitude as previously explained 
+    g <- graph_from_data_frame(df, directed = TRUE, vertices = meta)
+    
+    # Setting weights as the trade value 
+    g <- set_edge_attr(g, "weight", value = dt.trade.country.year$trade_value_usd)
+    
+    # Calculate KPIs and store in a data frame
+    # Select a vertex by name
+    country_name <- input$country
+    country_vertex <- V(g)[name == country_name]
+    
+    # Extract the corresponding row from the data frame
+    #  country_row <- df.map[country_vertex$index, ]
+    
+    df.map <- data.frame(
+      DegreeCentrality = degree(g),
+      ClosenessCentrality = closeness(g),
+      BetweennessCentrality = betweenness(g),
+      EigenvectorCentrality = eigen_centrality(g)$vector,
+      ClusteringCoefficient =transitivity(g)
+    )
+    # Transpose the table
+    df.map.country <- df.map[,country_vertex$index ]
+    df.map.t <- t(df.map)
+    
+    df.map.t
+  })
   
   # Data table output
   output$data_table <- renderDT({
